7c7bafdec1c0f0e08ed1fab18c3419d94502f130
maiharshrivastava <126783830+maiharshrivastava@users.noreply.github.com>
2023-10-28 22:09:35 +0530

    Add files via upload

---
+++ src/component/basics/menuApi.js
@@ -0,0 +1,96 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "Bedfordshire clanger",
+      price: "12₹",
+      description:
+        "Suet crust dumpling with a savoury filling one end, sweet filling the other. The savoury filling is usually meat with diced potatoes and vegetables. The sweet filling can be jam, cooked apple or other fruit. ",
+
+        popularity: 7,
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Bangers and mash",
+      category: "evening",
+      price: "20₹",
+      description:
+        "	Mashed potatoes and sausages, sometimes served with onion gravy or fried onions. Note that while sausages may date to the time given, potatoes are from the Americas and were not introduced to Europe until the 16th century. ",
+        popularity: 3,
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "Beans on toast	",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Tinned baked beans, heated, on toasted sliced bread.",
+        popularity: 1,
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "Chicken tikka masala",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Pieces of chicken tikka in a spiced creamy sauce",
+        popularity: 3,
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "Cobbler",
+      category: "evening",
+      price: "80₹",
+      description:
+        "  colonies	Fruit or savoury (e.g. beef) filling, covered with a scone mixture and baked",
+        popularity: 5,
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Beef Wellington",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "Beef cooked in a pastry crust ",
+        popularity: 6,
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Black pudding",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "	Blood sausage ",
+        popularity: 7,
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Cauliflower cheese	",
+      category: "lunch",
+      price: "60₹",
+      description:
+        " Cauliflower in a thick cheese sauce",
+        popularity: 2,
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Devilled kidneys",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Purple podded peas soaked overnight and simmered until mushy",
+        popularity: 4,
+    },
+  ];
+  
+  export default Menu;
